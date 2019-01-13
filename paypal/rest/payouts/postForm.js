a4a024ce055cd4e20a4e8e688cec619204d84848
Francisco Pereira da Silva <ciscoproo0@gmail.com>
2019-01-13 17:02:05 -0200

    Update postForm.js
    Fixed url

--- paypal/rest/payouts/postForm.js
+++ paypal/rest/payouts/postForm.js
@@ -1,5 +1,6 @@
 $('#payouts').submit(function(event){
     event.preventDefault();
+    var $form = $(this), url = $form.attr('action');
     var fields = $.post(url, {value: $('#value').val(), currency: $('#currency').val(), note: $('#note').val(), sender_item_id: $('#sender_item_id').val(), receiver: $('#receiver').val()});
     
     posting.done(function(data){
