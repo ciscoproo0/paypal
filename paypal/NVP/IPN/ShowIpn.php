0ad26b733053c7e1d27b5e85187545e65ad2022e
Francisco Pereira da Silva <ciscoproo0@gmail.com>
2019-01-25 22:08:27 -0200

    Improving IPN feature
    adding string expode

--- paypal/NVP/IPN/ShowIpn.php
+++ paypal/NVP/IPN/ShowIpn.php
@@ -3,5 +3,13 @@
 
     $ipn = $_SESSION['IPN'];
 
+    $responseNvp = array();
+ 
+    if (preg_match_all('/(?<name>[^\=]+)\=(?<value>[^&]+)&?/', $ipn, $matches)) {
+        foreach ($matches['name'] as $offset => $name) {
+            $responseNvp[$name] = $matches['value'][$offset];
+        }
+    }
+
     echo $ipn;
 ?>
